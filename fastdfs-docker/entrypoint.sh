911aeef7a4f006b0cdec6d4700c21c8012f42b68
devops <devops@gyenno.com>
2021-04-14 17:31:35 +0800

    fastdfs-docker优化
    1.增加自定义超时参数connect_timeout/network_timeout/max_connections/store_lookup/reserved_storage_space避免大量并发上传异常
    2.优化配置文件存在重复tracker问题

--- fastdfs-docker/entrypoint.sh
+++ fastdfs-docker/entrypoint.sh
@@ -15,6 +15,36 @@ tracker()
     then
         mkdir -p ${TRACKER_BASE_PATH}
     fi
+    if [ ! -z ${connect_timeout} ];then
+        sed -i "s:^connect_timeout.*=.*:connect_timeout=${connect_timeout}:g" /etc/fdfs/tracker.conf
+    else
+        sed -i "s:^connect_timeout.*=.*:connect_timeout=30:g" /etc/fdfs/tracker.conf
+    fi
+    if [ ! -z ${network_timeout} ];then
+        sed -i "s:^network_timeout.*=.*:network_timeout=${network_timeout}:g" /etc/fdfs/tracker.conf
+    else
+        sed -i "s:^network_timeout.*=.*:network_timeout=60:g" /etc/fdfs/tracker.conf
+    fi
+    if [ ! -z ${max_connections} ];then
+        sed -i "s:^max_connections.*=.*:max_connections=${max_connections}:g" /etc/fdfs/tracker.conf
+    else
+        sed -i "s:^max_connections.*=.*:max_connections=10240:g" /etc/fdfs/tracker.conf
+    fi
+    if [ ! -z ${store_lookup} ];then
+        sed -i "s:^store_lookup.*=.*:store_lookup=${store_lookup}:g" /etc/fdfs/tracker.conf
+    else
+        sed -i "s:^store_lookup.*=.*:store_lookup=2:g" /etc/fdfs/tracker.conf
+    fi
+    if [ ! -z ${reserved_storage_space} ];then
+        sed -i "s:^reserved_storage_space.*=.*:reserved_storage_space=${reserved_storage_space}%:g" /etc/fdfs/tracker.conf
+    else
+        sed -i "s:^reserved_storage_space.*=.*:reserved_storage_space=10%:g" /etc/fdfs/tracker.conf
+    fi
+    if [ ! -z ${log_file_keep_days} ];then
+        sed -i "s:^log_file_keep_days.*=.*:log_file_keep_days=${log_file_keep_days}:g" /etc/fdfs/tracker.conf
+    else
+        sed -i "s:^log_file_keep_days.*=.*:log_file_keep_days=7:g" /etc/fdfs/tracker.conf
+    fi
     echo "start fdfs_trackerd..."
     fdfs_trackerd ${TRACKER_CONF_FILE}
     tail -f "$TRACKER_LOG_FILE"
@@ -33,16 +63,30 @@ storage()
     if [ ! -z ${TRACKER_LIST} ]
     then
         sed -i "s/^.*tracker_server.*=.*$/#tracker_server/" /etc/fdfs/storage.conf
+        storage_line=$(sed -n '/^#tracker_server/=' /etc/fdfs/storage.conf |sed -n 1p)
         sed -i "s/^.*tracker_server.*=.*$/#tracker_server/" /etc/fdfs/mod_fastdfs.conf
+        mod_fastdfs_line=$(sed -n '/^#tracker_server/=' /etc/fdfs/mod_fastdfs.conf |sed -n 1p)
         TRACKER_LIST=($(echo ${TRACKER_LIST} | tr ';' ' '))
         for tracker in ${TRACKER_LIST[@]}
         do
             echo ${tracker}
             ping ${tracker} -c 2
-            sed -i "/#tracker_server/a\tracker_server=${tracker}:22122" /etc/fdfs/storage.conf
-            sed -i "/#tracker_server/a\tracker_server=${tracker}:22122" /etc/fdfs/mod_fastdfs.conf
+            sed -i "${storage_line}a\tracker_server=${tracker}:22122" /etc/fdfs/storage.conf
+            sed -i "${mod_fastdfs_line}a\tracker_server=${tracker}:22122" /etc/fdfs/mod_fastdfs.conf
+            # sed -i "/#tracker_server/a\tracker_server=${tracker}:22122" /etc/fdfs/storage.conf
+            # sed -i "/#tracker_server/a\tracker_server=${tracker}:22122" /etc/fdfs/mod_fastdfs.conf
         done
         sed -i "s:^group_name.*=.*:group_name=${GROUP_NAME}:g" /etc/fdfs/storage.conf
+        if [ ! -z ${connect_timeout} ];then
+            sed -i "s:^connect_timeout.*=.*:connect_timeout=${connect_timeout}:g" /etc/fdfs/mod_fastdfs.conf
+        else
+            sed -i "s:^connect_timeout.*=.*:connect_timeout=30:g" /etc/fdfs/mod_fastdfs.conf
+        fi
+        if [ ! -z ${network_timeout} ];then
+            sed -i "s:^network_timeout.*=.*:network_timeout=${network_timeout}:g" /etc/fdfs/mod_fastdfs.conf
+        else
+            sed -i "s:^network_timeout.*=.*:network_timeout=60:g" /etc/fdfs/mod_fastdfs.conf
+        fi
         sed -i "s:^group_name.*=.*:group_name=${GROUP_NAME}:g" /etc/fdfs/mod_fastdfs.conf
         GROUP_LIST=($(echo ${GROUP_LIST} | tr ';' ' '))
         sed -i "s:^group_count.*:group_count=${#GROUP_LIST[@]}:g" /etc/fdfs/mod_fastdfs.conf
@@ -56,6 +100,26 @@ store_path_count=1
 store_path0=/data/storage
 " >> /etc/fdfs/mod_fastdfs.conf
         done
+        if [ ! -z ${connect_timeout} ];then
+            sed -i "s:^connect_timeout.*=.*:connect_timeout=${connect_timeout}:g" /etc/fdfs/storage.conf
+        else
+            sed -i "s:^connect_timeout.*=.*:connect_timeout=30:g" /etc/fdfs/storage.conf
+        fi
+        if [ ! -z ${network_timeout} ];then
+            sed -i "s:^network_timeout.*=.*:network_timeout=${network_timeout}:g" /etc/fdfs/storage.conf
+        else
+            sed -i "s:^network_timeout.*=.*:network_timeout=60:g" /etc/fdfs/storage.conf
+        fi
+        if [ ! -z ${max_connections} ];then
+            sed -i "s:^max_connections.*=.*:max_connections=${max_connections}:g" /etc/fdfs/storage.conf
+        else
+            sed -i "s:^max_connections.*=.*:max_connections=10240:g" /etc/fdfs/storage.conf
+        fi
+        if [ ! -z ${log_file_keep_days} ];then
+            sed -i "s:^log_file_keep_days.*=.*:log_file_keep_days=${log_file_keep_days}:g" /etc/fdfs/storage.conf
+        else
+            sed -i "s:^log_file_keep_days.*=.*:log_file_keep_days=7:g" /etc/fdfs/storage.conf
+        fi
     fi
     fdfs_storaged "$STORAGE_CONF_FILE"
     /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
